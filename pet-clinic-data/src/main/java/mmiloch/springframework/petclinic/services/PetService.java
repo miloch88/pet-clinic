374bfd62d50287fe11ae1139c32cfdf8a96f9023
michal.miloch@gmail.com <6gLu3dsFFAA%93K9>
2020-10-02 15:08:09 +0200

    Refactor Services to Common Interface

--- pet-clinic-data/src/main/java/mmiloch/springframework/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/mmiloch/springframework/petclinic/services/PetService.java
@@ -1,15 +1,7 @@
 package mmiloch.springframework.petclinic.services;
 
-import mmiloch.springframework.petclinic.model.Owner;
 import mmiloch.springframework.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findByID(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/mmiloch/springframework/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/mmiloch/springframework/petclinic/services/VetService.java
@@ -1,15 +1,7 @@
 package mmiloch.springframework.petclinic.services;
 
-
 import mmiloch.springframework.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findByID(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
