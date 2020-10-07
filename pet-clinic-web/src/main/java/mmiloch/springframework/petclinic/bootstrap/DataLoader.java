95222d96f97ecebe26ece3e0e14a301492ba9922
michal.miloch@gmail.com <6gLu3dsFFAA%93K9>
2020-10-07 13:15:09 +0200

    Auto Generate Map IDs

--- pet-clinic-web/src/main/java/mmiloch/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mmiloch/springframework/petclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import mmiloch.springframework.petclinic.model.Owner;
 import mmiloch.springframework.petclinic.model.Vet;
 import mmiloch.springframework.petclinic.services.OwnerService;
 import mmiloch.springframework.petclinic.services.VetService;
-import mmiloch.springframework.petclinic.services.map.OwnerServiceMap;
-import mmiloch.springframework.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -31,7 +29,6 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Liliana");
         owner2.setLastName("Vess");
 
@@ -40,14 +37,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Gideon");
         vet1.setLastName("Jura");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Nissa");
         vet2.setLastName("Revane");
 
